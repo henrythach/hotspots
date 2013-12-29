ea40c7202b0f945b5f3a5ad20d515594960f4854
Henry Thach <thachhao@gmail.com>
2013-12-29 15:04:33 -0500

    Removed mongoose from routes

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/hotspots');
+// mongoose.connect('mongodb://localhost/hotspots');
 
-var Incident = mongoose.model('Incident', {
-    id: Number,
-    date: Date,
-    type: String,
-    location: String,
-    recorded: String,
-    callTaker: String,
-    disposition: String,
-    officer: String,
-    notes: String,
-    latitude: Number,
-    longitude: Number
-});
+// var Incident = mongoose.model('Incident', {
+//     id: Number,
+//     date: Date,
+//     type: String,
+//     location: String,
+//     recorded: String,
+//     callTaker: String,
+//     disposition: String,
+//     officer: String,
+//     notes: String,
+//     latitude: Number,
+//     longitude: Number
+// });
 
 exports.init = function(app) {
 
@@ -27,16 +27,16 @@ exports.init = function(app) {
         });
     });
 
-    /**
-     * Gets all incidents
-     */
-    app.get('/incidents.json', function(req, res) {
-        return Incident.where('latitude').exists(true).select('id date type location notes officer disposition latitude longitude').exec(function (err, incidents) {
-            if (!err) {
-                return res.send(incidents);
-            } else {
-                return console.log(err);
-            }
-        });
-    });
+    // /**
+    //  * Gets all incidents
+    //  */
+    // app.get('/incidents.json', function(req, res) {
+    //     return Incident.where('latitude').exists(true).select('id date type location notes officer disposition latitude longitude').exec(function (err, incidents) {
+    //         if (!err) {
+    //             return res.send(incidents);
+    //         } else {
+    //             return console.log(err);
+    //         }
+    //     });
+    // });
 };
