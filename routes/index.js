6a3f3156c6e62f45e642662c2cac88612e73c1ef
Henry Thach <thachhao@gmail.com>
2013-12-29 23:00:56 -0500

    Updated index.js with pg instead of mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,35 @@
-var mongoose = require('mongoose');
+/*global console,require*/
 
-mongoose.connect('mongodb://localhost/hotspots');
+(function (exports, pg) {
+    'use strict';
 
-var Incident = mongoose.model('Incident', {
-    id: Number,
-    date: Date,
-    type: String,
-    location: String,
-    recorded: String,
-    callTaker: String,
-    disposition: String,
-    officer: String,
-    notes: String,
-    latitude: Number,
-    longitude: Number
-});
+    exports.init = function (app) {
 
-exports.init = function(app) {
-
-    /**
-     * The homepage.  Just show index.jade
-     */
-    app.get('/', function(req, res) {
-        res.render('index', {
-            title: 'Hotspots Mapper'
+        /**
+         * The homepage.  Just show index.jade
+         */
+        app.get('/', function(req, res) {
+            res.render('index', {
+                title: 'Hotspots Mapper'
+            });
         });
-    });
 
-    /**
-     * Gets all incidents
-     */
-    app.get('/incidents.json', function(req, res) {
-        return Incident.where('latitude').exists(true).select('id date type location notes officer disposition latitude longitude').exec(function (err, incidents) {
-            if (!err) {
-                return res.send(incidents);
-            } else {
-                return console.log(err);
-            }
+        /**
+         * Gets all incidents
+         */
+        app.get('/incidents.json', function(req, res) {
+
+            pg.connect('postgres://localhost/hotspots', function (err, client, done) {
+                client.query('SELECT * FROM incident', function (err, result) {
+                    done();
+                    if (!err) {
+                        return res.send(result.rows);
+                    }
+                    return console.error(err);
+                });
+            });
         });
-    });
-};
+    };
+
+
+}(exports, require('pg')));
